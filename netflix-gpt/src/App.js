dd77e27325bed1ee76080d1b8d12479eb0dc990b
Your Name <your.name@example.com>
2024-07-28 19:13:20 +0530

    first commi

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl text-green-800 font-bold">
+      vgrghrygyy
+    </div>
+  );
+}
+
+export default App;
